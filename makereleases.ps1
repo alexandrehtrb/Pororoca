21f75fe7076c135527d33636dc73bd6ea9d9de0e
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2022-05-15 12:28:55 -0300

    cicd: refactored makereleases.ps1 into smaller functions; included Pororoca.Test package generation

--- makereleases.ps1
+++ makereleases.ps1
@@ -1,81 +1,195 @@
 # Stops the script run in case of any errors.
 $ErrorActionPreference = "Stop"
 
-Write-Host "Pororoca release maker." -ForegroundColor White
+########################## Pipeline #############################
 
-$now = (Get-Date -Format "dddd, dd/MM/yyyy HH:mm:ss K")
-Write-Host $now -ForegroundColor DarkGray
-
-[XML]$desktopCsprojXml = Get-Content ./src/Pororoca.Desktop/Pororoca.Desktop.csproj
-$versionName = $desktopCsprojXml.Project.PropertyGroup.Version
-Write-Host "Version name: ${versionName}" -ForegroundColor DarkGray
-
-<#
-$gitBranchName = (git branch --show-current)
-if ($gitBranchName -ne "release_candidate")
+function Run-Pipeline
 {
-	Write-Host "Releases need to be generated from the release_candidate branch." -ForegroundColor Red
-	Pause
-	Break
+	$runtimes = Get-RuntimesToPublishFor
+	$now = (Get-Date -Format "dddd, dd/MM/yyyy HH:mm:ss K")
+	$versionName = Read-VersionName
+	$sw = [System.Diagnostics.Stopwatch]::new()
+
+	Show-Greeting -VersionName $versionName -Now $now
+	Clean-Solution -Stopwatch $sw
+	Restore-Solution -Stopwatch $sw
+	Build-Solution -Stopwatch $sw
+	Run-UnitTests -Stopwatch $sw
+	Clean-OutputFolder -Stopwatch $sw
+
+	Generate-PororocaTestRelease -Stopwatch $sw
+	foreach ($runtime in $runtimes)
+	{
+		Generate-PororocaDesktopRelease -Stopwatch $sw -VersionName $versionName -Runtime $runtime
+	}
+	Show-Goodbye
 }
-$gitStatusCheck = (git status --porcelain)
-if ([string]::IsNullOrWhiteSpace($gitStatusCheck) -eq $false)
+
+function Get-RuntimesToPublishFor
 {
-	Write-Host "There are uncommited changes in this repository. Commit ou clear them before release." -ForegroundColor Red
-	Pause
-	Break
+	$unixRuntimes = @(`
+		'linux-x64' ` 
+		,'linux-arm64' `
+		,'osx-x64' `
+		,'osx-arm64' `
+	)
+	$windowsRuntimes = @(`
+		'win7-x64' ` 
+		,'win7-x86' `
+		,'win-x64' `
+		,'win-x86' `
+		,'win-arm' `
+		,'win-arm64' `
+	)
+
+	# Windows releases should be built on a Windows machine, because of dotnet
+	# Linux and Mac OS releases should be built on one of those OSs, because of chmod and zip
+	return $IsWindows ? $windowsRuntimes : $unixRuntimes
+	#return @("win7-x64")
 }
 
-git fetch
-#>
+#################### Pre-release build and tests ####################
 
-$stopwatch = [System.Diagnostics.Stopwatch]::new()
-
-Write-Host "Cleaning the solution..." -ForegroundColor DarkYellow
-$stopwatch.Start()
-dotnet clean --nologo --verbosity quiet
-$stopwatch.Stop()
-Write-Host "Solution cleaned ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
-
-Write-Host "Restoring the solution..." -ForegroundColor DarkYellow
-$stopwatch.Restart()
-dotnet restore --nologo --verbosity quiet
-$stopwatch.Stop()
-Write-Host "Solution restored ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
-
-Write-Host "Building the solution..." -ForegroundColor DarkYellow
-$stopwatch.Restart()
-dotnet build --configuration Release --nologo --verbosity quiet
-$stopwatch.Stop()
-Write-Host "Solution built ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
-
-Write-Host "Running unit tests..." -ForegroundColor DarkYellow
-$stopwatch.Restart()
-dotnet test --configuration Release --nologo --verbosity quiet
-$stopwatch.Stop()
-Write-Host "Solution tests run ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
-
-Write-Host "Deleting 'out' folder..." -ForegroundColor DarkYellow
-$stopwatch.Restart()
-Remove-Item "./out/" -Recurse -ErrorAction Ignore
-$stopwatch.Stop()
-Write-Host "Output folder deleted ($($stopwatch.Elapsed.Seconds)s)." -ForegroundColor DarkGreen
-
-$runtimes = @(`
-#'linux-x64' ` # Linux and Mac OS releases should be built on one of those OSs, because of chmod and zip
-#,'linux-arm64' `
-#,'osx-x64' `
-#,'osx-arm64' `
-'win7-x64' ` # Windows releases need to be built on a Windows machine, because of dotnet
-,'win7-x86' `
-,'win-x64' `
-,'win-x86' `
-,'win-arm' `
-,'win-arm64' `
-)
-
-foreach ($runtime in $runtimes)
+function Show-Greeting
 {
+    param (
+		[string]$versionName,
+		[string]$now
+    )
+	Write-Host "Pororoca release maker." -ForegroundColor White
+	Write-Host $now -ForegroundColor DarkGray
+	Write-Host "Version name: ${versionName}" -ForegroundColor DarkGray
+}
+
+function Show-Goodbye
+{
+	Write-Host "Finished!" -ForegroundColor DarkGreen
+}
+
+function Clean-Solution
+{
+	param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Cleaning the solution..." -ForegroundColor DarkYellow
+	$stopwatch.Restart()
+	dotnet clean --nologo --verbosity quiet
+	$stopwatch.Stop()
+	Write-Host "Solution cleaned ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
+}
+
+function Restore-Solution
+{
+	param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Restoring the solution..." -ForegroundColor DarkYellow
+	$stopwatch.Restart()
+	dotnet restore --nologo --verbosity quiet
+	$stopwatch.Stop()
+	Write-Host "Solution restored ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
+}
+
+function Build-Solution
+{
+	param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Building the solution..." -ForegroundColor DarkYellow
+	$stopwatch.Restart()
+	dotnet build --configuration Release --nologo --verbosity quiet
+	$stopwatch.Stop()
+	Write-Host "Solution built ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
+}
+
+function Run-UnitTests
+{
+	param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Running unit tests..." -ForegroundColor DarkYellow
+	$stopwatch.Restart()
+	dotnet test --configuration Release --nologo --verbosity quiet
+	$stopwatch.Stop()
+	Write-Host "Solution tests run ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
+}
+
+function Clean-OutputFolder
+{
+	param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Deleting 'out' folder..." -ForegroundColor DarkYellow
+	$stopwatch.Restart()
+	Remove-Item "./out/" -Recurse -ErrorAction Ignore
+	$stopwatch.Stop()
+	Write-Host "Output folder deleted ($($stopwatch.Elapsed.Seconds)s)." -ForegroundColor DarkGreen
+}
+
+#################### Release generation ####################
+
+function Read-VersionName
+{
+	[XML]$desktopCsprojXml = Get-Content ./src/Pororoca.Desktop/Pororoca.Desktop.csproj
+	$versionName = $desktopCsprojXml.Project.PropertyGroup.Version
+	return $versionName
+}
+
+function Generate-PororocaTestRelease {
+    param (
+        [System.Diagnostics.Stopwatch]$stopwatch
+    )
+
+	Write-Host "Publishing Pororoca.Test library..." -ForegroundColor DarkYellow
+
+	dotnet pack ./src/Pororoca.Test/Pororoca.Test.csproj `
+		--nologo `
+		--configuration Release
+
+	Write-Host "Package created! ($($stopwatch.Elapsed.Seconds)s)." -ForegroundColor DarkGreen
+}
+
+function Generate-PororocaDesktopRelease {
+    param (
+        [System.Diagnostics.Stopwatch]$stopwatch,
+		[string]$versionName,
+		[string]$runtime
+    )
+
+	$fullAppReleaseName = "Pororoca_${versionName}_${runtime}"
+	$outputFolder = "./out/${fullAppReleaseName}"
+	$zipName = "${fullAppReleaseName}.zip"
+	
+	Write-Host "Publishing Pororoca.Desktop for ${runtime}..." -ForegroundColor DarkYellow
+
+	$stopwatch.Restart()
+	
+	Publish-PororocaDesktop -Runtime $runtime -OutputFolder $outputFolder
+	Rename-Executable -Runtime $runtime -OutputFolder $outputFolder
+	Set-ExecutableAttributesIfUnix -Runtime $runtime -OutputFolder $outputFolder
+	Make-AppFolderIfMacOS -Runtime $runtime -OutputFolder $outputFolder
+	Copy-LogoIfLinux -Runtime $runtime -OutputFolder $outputFolder
+	Copy-Licence -OutputFolder $outputFolder
+	Compress-Package -OutputFolder $outputFolder -ZipName $zipName
+
+	$stopwatch.Stop()
+	Write-Host "Package created on ./out/${zipName} ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
+
+	Write-Host "SHA256 hash for package ${runtime}: $((Get-FileHash ./out/${zipName} -Algorithm SHA256).Hash)" -ForegroundColor DarkGreen
+}
+
+function Publish-PororocaDesktop
+{
+	param (
+		[string]$runtime,
+		[string]$outputFolder
+    )
+
 	if ($runtime -like "*win7*")
 	{
 		$publishSingleFile = $False
@@ -85,14 +199,7 @@ foreach ($runtime in $runtimes)
 		$publishSingleFile = $True
 	}
 
-	$fullAppReleaseName = "Pororoca_${versionName}_${runtime}"
-	$outputFolder = "./out/${fullAppReleaseName}"
-	$zipName = "${fullAppReleaseName}.zip"
 	$publishSingleFileArg = $(${publishSingleFile}.ToString().ToLower())
-	
-	Write-Host "Publishing for ${runtime}..." -ForegroundColor DarkYellow
-	
-	$stopwatch.Restart()
 	
 	dotnet publish ./src/Pororoca.Desktop/Pororoca.Desktop.csproj `
 		--verbosity quiet `
@@ -102,7 +209,15 @@ foreach ($runtime in $runtimes)
 		--self-contained true `
 		--runtime $runtime `
 		--output $outputFolder
-	
+}
+
+function Rename-Executable
+{
+	param (
+		[string]$runtime,
+		[string]$outputFolder
+    )
+
 	if ($runtime -like "*win*")
 	{
 		Rename-Item -Path "${outputFolder}/Pororoca.Desktop.exe" -NewName "Pororoca.exe"
@@ -110,7 +225,19 @@ foreach ($runtime in $runtimes)
 	else
 	{
 		Rename-Item "${outputFolder}/Pororoca.Desktop" -NewName "Pororoca"
-		
+	}
+}
+
+
+function Set-ExecutableAttributesIfUnix
+{
+	param (
+		[string]$runtime,
+		[string]$outputFolder
+    )
+
+	if ($runtime -notlike "*win*")
+	{
 		# If this is script is running on Linux or Mac OS,
 		# lets also set UNIX executable attribute for the program,
 		# if the target runtime is Linux or Mac OS
@@ -123,6 +250,14 @@ foreach ($runtime in $runtimes)
 			Write-Host "Program file will not have UNIX executable attribute.`nRemember to run 'chmod +x' before running." -ForegroundColor Magenta
 		}
 	}
+}
+
+function Make-AppFolderIfMacOS
+{
+	param (
+		[string]$runtime,
+		[string]$outputFolder
+    )
 
 	if ($runtime -like "*osx*")
 	{
@@ -139,6 +274,14 @@ foreach ($runtime in $runtimes)
 		Get-ChildItem $outputFolder -File | Copy-Item -Destination "${outputFolder}/Pororoca.app/Contents/MacOS/" -Force
 		Get-ChildItem $outputFolder -File | Remove-Item
 	}
+}
+
+function Copy-LogoIfLinux
+{
+	param (
+		[string]$runtime,
+		[string]$outputFolder
+    )
 
 	if ($runtime -like "*linux*")
 	{
@@ -146,10 +289,24 @@ foreach ($runtime in $runtimes)
 		Copy-Item -Path "./pororoca.png" `
 			  -Destination $outputFolder
 	}
+}
+
+function Copy-Licence
+{
+	param (
+		[string]$outputFolder
+    )
 
 	Copy-Item -Path "./LICENCE.md" `
 			  -Destination $outputFolder
+}
 
+function Compress-Package
+{
+	param (
+		[string]$outputFolder,
+		[string]$zipName
+    )
 
 	if ($IsWindows)
 	{
@@ -166,12 +323,8 @@ foreach ($runtime in $runtimes)
 	}
 
 	Remove-Item $outputFolder -Force -Recurse -ErrorAction Ignore
-	
-	$stopwatch.Stop()
-	Write-Host "Package created on ./out/${zipName} ($($stopwatch.Elapsed.TotalSeconds.ToString("#"))s)." -ForegroundColor DarkGreen
-
-	Write-Host "SHA256 hash for package ${runtime}: $((Get-FileHash ./out/${zipName} -Algorithm SHA256).Hash)" -ForegroundColor DarkGreen
-
 }
 
-Write-Host "Finished!" -ForegroundColor DarkGreen
+########################## Execute #############################
+
+Run-Pipeline
