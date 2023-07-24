c62f4fa8d53a5a43104708a5ff956912dd9d5d11
Alexandre H. T. R. Bonfitto <alexandrehtrb@outlook.com>
2023-07-24 19:10:14 -0300

    feature: adds move up/down on collection and environment variables tables

--- src/Pororoca.Desktop/ViewModels/VariableViewModel.cs
+++ src/Pororoca.Desktop/ViewModels/VariableViewModel.cs
@@ -24,6 +24,10 @@ public class VariableViewModel : ViewModelBase
 
     public ReactiveCommand<Unit, Unit> RemoveVariableCmd { get; }
 
+    public ReactiveCommand<Unit, Unit> MoveVariableUpCmd { get; }
+
+    public ReactiveCommand<Unit, Unit> MoveVariableDownCmd { get; }
+
     public VariableViewModel(ObservableCollection<VariableViewModel> parentCollection, PororocaVariable v)
         : this(parentCollection, v.Enabled, v.Key, v.Value ?? string.Empty, v.IsSecret)
     {
@@ -38,6 +42,8 @@ public class VariableViewModel : ViewModelBase
         Value = value;
         IsSecret = isSecret;
         RemoveVariableCmd = ReactiveCommand.Create(RemoveVariable);
+        MoveVariableUpCmd = ReactiveCommand.Create(MoveVariableUp);
+        MoveVariableDownCmd = ReactiveCommand.Create(MoveVariableDown);
     }
 
     public PororocaVariable ToVariable() =>
@@ -45,4 +51,27 @@ public class VariableViewModel : ViewModelBase
 
     public void RemoveVariable() =>
         this.parentCollection.Remove(this);
+
+    private void MoveVariableUp()
+    {
+        int oldIndex = this.parentCollection.IndexOf(this);
+        if (oldIndex == 0) return;
+        int newIndex = oldIndex - 1;
+        MoveVariable(oldIndex, newIndex);
+    }
+
+    private void MoveVariableDown()
+    {
+        int oldIndex = this.parentCollection.IndexOf(this);
+        if (oldIndex == this.parentCollection.Count - 1) return;
+        int newIndex = oldIndex + 1;
+        MoveVariable(oldIndex, newIndex);
+    }
+
+    private void MoveVariable(int oldIndex, int newIndex)
+    {
+        // For some reason, ObservableCollection.Move() does not work here with DataGrid
+        this.parentCollection.RemoveAt(oldIndex);
+        this.parentCollection.Insert(newIndex, this);
+    }
 }
