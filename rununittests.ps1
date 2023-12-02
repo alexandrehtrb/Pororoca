af37294930f523ce4a988248be81de2bce04c317
Alexandre H. T. R. Bonfitto <alexandrehtrb@outlook.com>
2023-12-02 09:29:56 -0300

    fix: unit tests coverage report ignore source generated files

--- rununittests.ps1
+++ rununittests.ps1
@@ -4,4 +4,8 @@
 
 Remove-Item "./TestResults/" -Recurse -ErrorAction Ignore
 dotnet test --collect:"XPlat Code Coverage" --results-directory "./TestResults/" --filter FullyQualifiedName!~Pororoca.Test.Tests
-reportgenerator "-reports:./TestResults/**/coverage.cobertura.xml" "-targetdir:./TestResults/" "-assemblyfilters:+Pororoca.Domain;+Pororoca.Domain.OpenAPI" -reporttypes:Html
+reportgenerator "-reports:./TestResults/**/coverage.cobertura.xml" `
+                "-targetdir:./TestResults/" `
+                "-assemblyfilters:+Pororoca.Domain;+Pororoca.Domain.OpenAPI" `
+                "-filefilters:-*.g.cs" `
+                "-reporttypes:Html"
