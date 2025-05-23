d02b36256834dd5b37951c04bb1d278119b86d9d
Alexandre H.T.R. Bonfitto <alexandrehtrb@outlook.com>
2025-05-23 16:07:24 -0300

    refactor: include inside random CPF / CNPJ methods digits logic

--- src/Pororoca.Domain/Features/VariableResolution/PororocaPredefinedVariableEvaluator.cs
+++ src/Pororoca.Domain/Features/VariableResolution/PororocaPredefinedVariableEvaluator.cs
@@ -61,11 +61,28 @@ public static partial class PororocaPredefinedVariableEvaluator
 
     private static string GetRandomQuantity() => Random.Shared.Next(1, 1001).ToString();
 
-    #region CPF
-
     private static string GetRandomCPF(bool includeSeparators)
     {
-        ReadOnlySpan<byte> digits = GetRandomCPFDigits();
+        static int CalculateVerifierDigit(ReadOnlySpan<byte> digits)
+        {
+            int sum = 0;
+            int factor = 2;
+            for (int x = digits.Length - 1; x >= 0; x--)
+            {
+                sum += factor * digits[x];
+                factor++;
+            }
+            int mod = sum % 11;
+            return mod < 2 ? 0 : (11 - mod);
+        }
+
+        Span<byte> digits = stackalloc byte[11];
+        Random.Shared.NextBytes(digits[0..9]);
+        for (int i = 0; i < 9; i++) digits[i] = (byte)(digits[i] % 10);
+
+        digits[9] = (byte)CalculateVerifierDigit(digits[0..9]);
+        digits[10] = (byte)CalculateVerifierDigit(digits[0..10]);
+
         StringBuilder sb = new(14);
         sb.Append(digits[0]);
         sb.Append(digits[1]);
@@ -84,7 +101,7 @@ public static partial class PororocaPredefinedVariableEvaluator
         return sb.ToString();
     }
 
-    private static ReadOnlySpan<byte> GetRandomCPFDigits()
+    private static string GetRandomCNPJ(bool includeSeparators)
     {
         static int CalculateVerifierDigit(ReadOnlySpan<byte> digits)
         {
@@ -94,28 +111,24 @@ public static partial class PororocaPredefinedVariableEvaluator
             {
                 sum += factor * digits[x];
                 factor++;
+                if (factor > 9)
+                    factor = 2; // DIFERENTE DO CPF
             }
             int mod = sum % 11;
             return mod < 2 ? 0 : (11 - mod);
         }
 
-        Span<byte> digits = stackalloc byte[11];
-        Random.Shared.NextBytes(digits[0..9]);
-        for (int i = 0; i < 9; i++) digits[i] = (byte) (digits[i] % 10);
+        Span<byte> digits = stackalloc byte[14];
+        Random.Shared.NextBytes(digits[0..8]);
+        for (int i = 0; i < 8; i++) digits[i] = (byte)(digits[i] % 10);
+        digits[8] = 0;
+        digits[9] = 0;
+        digits[10] = 0;
+        digits[11] = 1;
 
-        digits[9] = (byte) CalculateVerifierDigit(digits[0..9]);
-        digits[10] = (byte) CalculateVerifierDigit(digits[0..10]);
+        digits[12] = (byte)CalculateVerifierDigit(digits[0..12]);
+        digits[13] = (byte)CalculateVerifierDigit(digits[0..13]);
 
-        return digits.ToArray();
-    }
-
-    #endregion
-
-    #region CNPJ
-
-    private static string GetRandomCNPJ(bool includeSeparators)
-    {
-        ReadOnlySpan<byte> digits = GetRandomCNPJDigits();
         StringBuilder sb = new(18);
         sb.Append(digits[0]);
         sb.Append(digits[1]);
@@ -137,36 +150,4 @@ public static partial class PororocaPredefinedVariableEvaluator
         sb.Append(digits[13]);
         return sb.ToString();
     }
-
-    private static ReadOnlySpan<byte> GetRandomCNPJDigits()
-    {
-        static int CalculateVerifierDigit(ReadOnlySpan<byte> digits)
-        {
-            int sum = 0;
-            int factor = 2;
-            for (int x = digits.Length - 1; x >= 0; x--)
-            {
-                sum += factor * digits[x];
-                factor++;
-                if (factor > 9) factor = 2; // DIFERENTE DO CPF
-            }
-            int mod = sum % 11;
-            return mod < 2 ? 0 : (11 - mod);
-        }
-
-        Span<byte> digits = stackalloc byte[14];
-        Random.Shared.NextBytes(digits[0..8]);
-        for (int i = 0; i < 8; i++) digits[i] = (byte) (digits[i] % 10);
-        digits[8] = 0;
-        digits[9] = 0;
-        digits[10] = 0;
-        digits[11] = 1;
-
-        digits[12] = (byte) CalculateVerifierDigit(digits[0..12]);
-        digits[13] = (byte) CalculateVerifierDigit(digits[0..13]);
-
-        return digits.ToArray();
-    }
-
-    #endregion
 }
